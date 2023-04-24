241004ea180cc392c4fc35486a566bd1ad2bf655
MohdBasurra <mohdye96@gmail.com>
2023-04-24 14:13:02 +0300

    assigment completed

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,10 @@
-import React from "react";
+import {React, useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [toggle, setToggle] = useState(false);
+//on and off
+  return <button onClick={setToggle(!Toggle)}>{toggle ? "on" : "off" }
+  {toggle}</button>;
 }
 
 export default Toggle;
